ae46051fb0ddbc01b9635ab935b240807fca5c41
Serhii Stanislav <serhiistanislav@gmail.com>
2024-10-13 16:14:22 +0300

    ADD | init Get controllers

--- src/tokens/tokens.controller.ts
+++ src/tokens/tokens.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tokens')
-export class TokensController {}
+export class TokensController {
+  @Get()
+  findAll() {
+    return [1, 2, 3];
+  }
+
+  @Get(':symbol')
+  findOne(@Param('symbol') symbol: string) {
+    return { symbol };
+  }
+}
