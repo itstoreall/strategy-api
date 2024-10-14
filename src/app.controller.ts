fe34c95aa4a80e09aa1c5f9e566c56875ac751a1
Serhii Stanislav <serhiistanislav@gmail.com>
2024-10-14 20:54:48 +0300

    ADD | Throttler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getRoot(): string {
+  @Get('/')
+  init(): string {
+    return this.appService.init();
+  }
+
+  @Get('/api')
+  api(): string {
     return this.appService.init();
   }
 }
