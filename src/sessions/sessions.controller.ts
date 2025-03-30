07bfe7bd6640c6bdc5f66676bf5082ac4577d6ee
Serhii Stanislav <serhiistanislav@gmail.com>
2025-03-30 18:49:33 +0300

    EDIT | code refactoring

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -3,35 +3,4 @@ import { Controller } from '@nestjs/common';
 // import { LoggerService } from '../logger/logger.service';
 
 @Controller('sessions')
-export class SessionsController {
-  // constructor(private readonly sessionsService: SessionsService) {}
-  // private readonly logger = new LoggerService(SessionsController.name);
-  /*
-  @Get('/all')
-  getAllSessions(
-    @Query('userId') userId: string,
-    @Query('sessionToken') sessionToken: string,
-    @Ip() ip: string,
-  ) {
-    this.logger.log(
-      `Req for Session ${userId}\t${ip}`,
-      SessionsController.name,
-    );
-    return this.sessionsService.getAllSessions(userId, sessionToken);
-  }
-  */
-  /*
-  @Get(':id')
-  findOne(
-    @Param('userId') userId: string,
-    @Query('sessionToken') sessionToken: string,
-    @Ip() ip: string,
-  ) {
-    this.logger.log(
-      `Req for Session ${userId}\t${ip}`,
-      SessionsController.name,
-    );
-    return this.sessionsService.findByUserId(userId);
-  }
-  */
-}
+export class SessionsController {}
